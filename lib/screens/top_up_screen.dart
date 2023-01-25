9410cacfebe6278219a51511c88deca62ebd3a4f
winxene <winxeneric@gmail.com>
2023-01-25 22:04:22 +0700

    feat: added url_launcher feature

--- lib/screens/top_up_screen.dart
+++ lib/screens/top_up_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
+import 'package:url_launcher/url_launcher.dart';
 import 'package:trmade/widgets/bottom_nav_bar.dart';
 
 import 'package:trmade/components/snack_bar.dart';
@@ -8,6 +8,16 @@ import 'package:trmade/components/snack_bar.dart';
 class TopUpScreen extends StatelessWidget {
   static const routeName = '/top_up';
   String virtualAccountNumber = '0000-xxxx-2736-yyyy-8882-6633';
+  String drinkifyURL = 'https://drinkify-website.vercel.app/';
+
+  _launchURL() async {
+    final uri = Uri.parse(drinkifyURL);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      throw 'Could not launch $drinkifyURL';
+    }
+}
 
   @override
   Widget build(BuildContext context) {
@@ -35,15 +45,40 @@ class TopUpScreen extends StatelessWidget {
           Container(
             margin: EdgeInsets.symmetric(horizontal: 40),
             child: Text(
-              'To Top-up, please transfer via virtual account in your bank account.',
+              'To Top-up, please visit the link below and get the token code. Then, enter the token code in the field below.',
               style: TextStyle(
                 color: Colors.white,
                 fontSize: 12,
               ),
             ),
           ),
-          SizedBox(
-            height: 20,
+          ElevatedButton(
+            onPressed: () {
+              _launchURL();
+            },
+            child: Text(
+              'Visit Drinkify Website',
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            style: ElevatedButton.styleFrom(
+              padding: EdgeInsets.only(
+                left: 70,
+                right: 70,
+                top: 10,
+                bottom: 10,
+              ),
+              primary: Color.fromRGBO(0, 191, 166, 1), //button color
+              onPrimary:
+                  Colors.white, //inside button color, in this case Text widget
+              minimumSize: Size(120,
+                  25), //increase the minimum size of the button (width, height)
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
           ),
           Container(
             margin: EdgeInsets.only(right: 115),
