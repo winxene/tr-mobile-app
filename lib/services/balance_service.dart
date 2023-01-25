32a3f84a1ad0ef14525fa43104dc907c1f6909c0
winxene <winxeneric@gmail.com>
2023-01-26 01:25:10 +0700

    chore: refactoring some code

--- lib/services/balance_service.dart
+++ lib/services/balance_service.dart
@@ -11,49 +11,62 @@ import 'auth_service.dart';
 // Look up the balance for the user in the database
 DatabaseReference ref = FirebaseDatabase.instance.ref("users");
 DatabaseReference topUpRef = FirebaseDatabase.instance.ref("balanceToken");
-class BalanceService extends ChangeNotifier{
+
+class BalanceService extends ChangeNotifier {
   int _balance = 0;
   int get balance => _balance;
-
-  set balance(int newBalance){
+  set balance(int newBalance) {
+    getBalance();
     _balance = newBalance;
     notifyListeners();
   }
 
-  Future <void> updateBalance(int amount, bool isAdd, String tokenEntered) async{
+  Future<void> addBalance(
+      int amount, String tokenEntered) async {
     await topUpRef.child("5k").once().then((event) async {
       var snapshot = event.snapshot;
-      if(snapshot.value != null){
+      if (snapshot.value != null) {
         var token = snapshot.value as String;
-        if(token == tokenEntered){
+        if (token == tokenEntered) {
           String userID = AuthService(FirebaseAuth.instance).user.uid;
           await getBalance();
-          isAdd? _balance +=amount : _balance -= amount;
+          _balance += amount;
           ref.update({userID: _balance});
-          notifyListeners();    
-          topUpRef.update({"refresh" : true});
+          notifyListeners();
+          topUpRef.update({"refresh": true});
+          await getBalance();
         }
-      }
-    }
-    );
-  }
-
-  Future <void> getBalance() async{
-    String userID = AuthService(FirebaseAuth.instance).user.uid;
-    await ref.child(userID).once().then((event) {
-      var snapshot = event.snapshot;
-      if(snapshot.value != null){
-      _balance = snapshot.value as int;
-      notifyListeners();
       }
     });
   }
 
-  Future <void> getUserInfo() async {
+  Future<void> deductBalance(int amount) async{
+    await getBalance();
+    if(_balance>=amount){
+      _balance -= amount;
+      String userID = AuthService(FirebaseAuth.instance).user.uid;
+      ref.update({userID: _balance});
+      notifyListeners();
+      await getBalance();
+    }
+  }
+
+  Future<void> getBalance() async {
+    String userID = AuthService(FirebaseAuth.instance).user.uid;
+    await ref.child(userID).once().then((event) {
+      var snapshot = event.snapshot;
+      if (snapshot.value != null) {
+        _balance = snapshot.value as int;
+        notifyListeners();
+      }
+    });
+  }
+
+  Future<void> getUserInfo() async {
     String userID = AuthService(FirebaseAuth.instance).user.uid;
     bool sameUserFound = false;
-    await ref.get().then((snapshot) async{
-      for(final userIDs in snapshot.children) {
+    await ref.get().then((snapshot) async {
+      for (final userIDs in snapshot.children) {
         if (userID == userIDs.key) {
           sameUserFound = true;
           break;
@@ -63,7 +76,8 @@ class BalanceService extends ChangeNotifier{
         _balance = 0;
         ref.child(userID).set(_balance);
         notifyListeners();
-      } else  {
+        getBalance();
+      } else {
         // _balance = snapshot.value[userID] as int;
         await getBalance();
         notifyListeners();
@@ -71,4 +85,3 @@ class BalanceService extends ChangeNotifier{
     });
   }
 }
-
